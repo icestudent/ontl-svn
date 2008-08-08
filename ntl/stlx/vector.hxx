90966770f891d661208c403f9724aae51e45189b
freak.fsfoundry.org <freak.fsfoundry.org@2fbf614b-5b33-0410-b9e2-9946931d7d41>
2008-08-08 10:42:22 +0000

    Exception-safe and iterator aware version of vector's constructors and vector::assign overloads. Minor change to basic_string.

--- ntl/stlx/vector.hxx
+++ ntl/stlx/vector.hxx
@@ -32,6 +32,254 @@ namespace std {
  *    all of the same type, into a strictly linear arrangement.
  */
 
+namespace detail
+{
+
+// There should be a better place for things in the detail namespace.
+// !fr3@K!
+
+template <class ForwardIterator, class Allocator>
+struct range_construct_guard
+{
+  typedef ForwardIterator iterator;
+  typedef Allocator allocator_type;
+  typedef typename iterator_traits<ForwardIterator>::value_type value_type;
+
+  range_construct_guard(iterator first, allocator_type& alloc) __ntl_nothrow
+  : first_(first),
+    current_(first),
+    alloc_(alloc),
+    committed_(false)
+  {
+    // Making sure users don't get the wrong combinations of iterator
+    // and allocator.
+    // Consider using concepts instead of static assertion in the
+    // future.
+    // !fr3@K!
+    typedef value_type iterator_value_type;
+    typedef typename allocator_type::value_type allocator_value_type;
+    STATIC_ASSERT((is_same<iterator_value_type, allocator_value_type>::value));
+  }
+  ~range_construct_guard() __ntl_nothrow
+  {
+    if(committed_)
+      return;
+
+    for(; first_ != current_; ++first_)
+      alloc_.destroy(first_);
+  }
+
+  void construct_from(const value_type& value)
+  {
+    alloc_.construct(current_, value);
+    ++current_;
+  }
+
+  iterator commit()
+  {
+    committed_ = true;
+    return current_;
+  }
+
+private:
+  // no value semantics
+  range_construct_guard(const range_construct_guard&);
+  range_construct_guard& operator=(const range_construct_guard&);
+
+private:
+  iterator first_;
+  iterator current_;
+  allocator_type& alloc_;
+  bool committed_;
+};
+
+template <class ForwardIterator, class T, class Allocator>
+void uninitailized_fill_a(ForwardIterator first,
+                          ForwardIterator last,
+                          const T& value,
+                          Allocator& alloc)
+{
+  range_construct_guard<ForwardIterator, Allocator> g(first, alloc);
+  for(; first != last; ++first)
+  {
+    g.construct_from(value);
+  }
+  g.commit();
+  assert(first == g.commit());
+}
+
+template <class ForwardIterator, class Size, class T, class Allocator>
+void uninitailized_fill_n_a(ForwardIterator first,
+                            Size n,
+                            const T& value,
+                            Allocator& alloc)
+{
+  range_construct_guard<ForwardIterator, Allocator> g(first, alloc);
+  for(; n; --n, ++first)
+  {
+    g.construct_from(value);
+  }
+  g.commit();
+  assert(first == g.commit());
+}
+
+template <class InputIterator, class ForwardIterator, class Allocator>
+InputIterator uninitailized_copy_a(InputIterator first,
+                                   InputIterator last,
+                                   ForwardIterator dst,
+                                   Allocator& alloc)
+{
+  range_construct_guard<ForwardIterator, Allocator> g(dst, alloc);
+  for(; first != last; ++first, ++dst)
+  {
+    g.construct_from(*first);
+  }
+  return g.commit();
+}
+
+template <class InputIterator,
+          class Size,
+          class ForwardIterator,
+          class Allocator>
+InputIterator uninitailized_copy_n_a(InputIterator first,
+                                     Size n,
+                                     ForwardIterator dst,
+                                     Allocator& alloc)
+{
+  range_construct_guard<ForwardIterator, Allocator> g(dst, alloc);
+  for(; n; ++first, ++dst)
+  {
+    g.construct_from(*first);
+  }
+  return g.commit();
+}
+
+// Next Largest Power of 2, optionally used in
+// vector_allocation_policy().
+// Ref: http://www.aggregate.org/MAGIC/#Next%20Largest%20Power%20of%202
+template <size_t Size>
+struct nlpo2_impl
+{
+};
+
+template <>
+struct nlpo2_impl<32>
+{
+  static uint32_t get(uint32_t n)
+  {
+    n |= (n >> 1);
+    n |= (n >> 2);
+    n |= (n >> 4);
+    n |= (n >> 8);
+    n |= (n >> 16);
+    return(++n);
+  }
+};
+template <>
+struct nlpo2_impl<64>
+{
+  static uint64_t get(uint64_t n)
+  {
+    n |= (n >> 1);
+    n |= (n >> 2);
+    n |= (n >> 4);
+    n |= (n >> 8);
+    n |= (n >> 16);
+    n |= (n >> 32);
+    return(++n);
+  }
+};
+
+size_t nlpo2(size_t n)
+{
+  return nlpo2_impl<sizeof(size_t) * 8>::get(n);
+}
+
+//#define NTL__VECTOR_ALLOCATION_POLICY__USER_DEFINED
+//#define NTL__VECTOR_ALLOCATION_POLICY__AGGRESSIVE
+//#define NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE
+
+// Makes sure no more than one vector allocation policy is defined.
+#if (defined(NTL__VECTOR_ALLOCATION_POLICY__USER_DEFINED) && \
+    (defined(NTL__VECTOR_ALLOCATION_POLICY__AGGRESSIVE) || \
+     defined(NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE))) || \
+     (defined(NTL__VECTOR_ALLOCATION_POLICY__AGGRESSIVE) && \
+      defined(NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE))
+# error Only one vector capacity policy is allowed!
+#endif
+
+// Defines default vector allocation behavior, if none defined.
+#if !defined(NTL__VECTOR_ALLOCATION_POLICY__USER_DEFINED) && \
+    !defined(NTL__VECTOR_ALLOCATION_POLICY__AGGRESSIVE) && \
+    !defined(NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE)
+# define NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE
+#endif
+
+// Interface to vector allocation policy.
+size_t vector_allocation_policy(size_t min)
+{
+#if defined(NTL__VECTOR_ALLOCATION_POLICY__USER_DEFINED)
+  // user with user-defined policy must declare their policy as:
+  return ::ntl_user_defined::vector_allocation_policy(min);
+
+#elif defined(NTL__VECTOR_ALLOCATION_POLICY__AGGRESSIVE)
+  return nlpo2(min);
+
+#else // NTL__VECTOR_ALLOCATION_POLICY__CONSERVATIVE
+  return min;
+#endif
+}
+
+template <bool>
+struct make_bool
+{
+  typedef false_type value;
+};
+template <>
+struct make_bool<true>
+{
+  typedef true_type value;
+};
+
+template <class Allocator>
+struct allocation_guard
+{
+  typedef Allocator allocator_type;
+  typedef typename Allocator::pointer pointer;
+
+  allocation_guard(allocator_type& alloc, size_t n)
+  : alloc_(alloc),
+    n_(n),
+    p_(alloc.allocate(n))
+  {
+  }
+  ~allocation_guard()
+  {
+    if(p_ == nullptr)
+      return;
+
+    alloc_.deallocate(p_, n_);
+  }
+
+  pointer get() const
+  {
+    assert(p_ != nullptr);
+    return p_;
+  }
+
+  void commit()
+  {
+    p_ = nullptr;
+  }
+
+  private:
+    allocator_type& alloc_;
+    size_t n_;
+    pointer p_;
+};
+
+} // namespace detail
+
 /// Class template vector [23.2.6]
 template <class T, class Allocator = allocator<T> >
 class vector
@@ -56,11 +304,12 @@ class vector
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
   private:
+    typedef detail::allocation_guard<allocator_type> allocation_guard;
 
     void construct(size_type n, const T& value)
     {
       iterator i = begin_;
-      while ( n-- ) allocator_.construct(i++, value);
+      while ( n-- ) alloc_.construct(i++, value);
       end_ = i;
     }
 
@@ -70,27 +319,32 @@ class vector
     {
       iterator i = begin_;
       for ( ; n--; ++first, ++i )
-        allocator_.construct(i, *first);
+        alloc_.construct(i, *first);
       end_ = i;
     }
 
-    template <class ForwardIterator>
-    void vector__disp(ForwardIterator first, ForwardIterator last,
-                      const false_type&)
+    template <class InputIterator>
+    void vector__disp(InputIterator first, InputIterator last,
+                      const input_iterator_tag)
     {
-      ///\todo specialize for InputIterator category
-      // distance SHOULD be allways positive
-      capacity_ = static_cast<size_type>(distance(first, last));
-      begin_ = allocator_.allocate(capacity_);
-      construct(first, capacity_);
+      capacity_ = 0;
+      begin_ = end_ = nullptr;
+
+      for(; first != last; ++first)
+        this->push_back(*first);
     }
 
-    template <class IntegralType>
-    void vector__disp(IntegralType n, IntegralType x, const true_type&)
+    template <class ForwardIterator, class IteratorCategory>
+    void vector__disp(ForwardIterator first, ForwardIterator last,
+                      const IteratorCategory)
     {
-      capacity_ = static_cast<size_type>(n);
-      begin_ = allocator_.allocate(capacity_);
-      construct(static_cast<size_type>(n), static_cast<value_type>(x));
+      size_type n = distance(first, last);
+      capacity_ = detail::vector_allocation_policy(n);
+      allocation_guard g(alloc_, capacity_);
+      begin_ = end_ = g.get();
+      detail::uninitailized_copy_a(first, last, begin_, alloc_);
+      g.commit();
+      end_ += n;
     }
 
   public:
@@ -98,51 +352,62 @@ class vector
     ///\name construct/copy/destroy [23.2.6.1]
 
     explicit vector(const Allocator& a = Allocator())
-    : allocator_(a), begin_(0), end_(0), capacity_(0) {}
+    : alloc_(a), capacity_(0), begin_(nullptr), end_(nullptr) {}
 
-    explicit vector(      size_type n,
+    explicit vector(size_type n,
                     const T& value      = T(),
                     const Allocator& a  = Allocator())
-    : allocator_(a)
+    : alloc_(a),
+      capacity_(detail::vector_allocation_policy(n)),
+      begin_(alloc_.allocate(capacity_)),
+      end_(begin_)
     {
-      capacity_ = n;
-      begin_= allocator_.allocate(n);
-      construct(n, value);
+      detail::uninitailized_fill_n_a(begin_, n, value, alloc_);
+      end_ += n;
     }
 
     template <class InputIterator>
     vector(InputIterator first,
            InputIterator last,
            const Allocator& a = Allocator())
-    : allocator_(a)
+    : alloc_(a)
     {
-      vector__disp(first, last, is_integral<InputIterator>::type());
+      vector__disp(
+        first,
+        last,
+        typename iterator_traits<InputIterator>::iterator_category());
     }
 
     __forceinline
     vector(const vector<T, Allocator>& x)
-    : allocator_(x.allocator_)
+    : alloc_(x.alloc_),
+      capacity_(detail::vector_allocation_policy(x.size()))
     {
-      capacity_ = x.size();
       if ( !capacity_ )
-        begin_ = end_ = 0;
-      else
       {
-        begin_ = allocator_.allocate(capacity_);
-        construct(x.begin(), capacity_);
+        begin_ = end_ = nullptr;
+        return;
       }
+
+      allocation_guard g(alloc_, capacity_);
+      begin_ = end_ = g.get();
+      detail::uninitailized_copy_a(x.begin(), x.end(), begin_, alloc_);
+      g.commit();
+      end_ += x.size();
     }
 
     __forceinline
     ~vector() __ntl_nothrow
     {
+      if(capacity_ == 0)
+        return;
+
       clear();
-      if ( begin_ ) allocator_.deallocate(begin_, capacity_);
+      alloc_.deallocate(begin_, capacity_);
     }
 
     vector<T, Allocator>& operator=(const vector<T, Allocator>& x)
     {
-    //?  allocator_ = x.allocator_;
       assign(x.begin(), x.end());
       return *this;
     }
@@ -150,7 +415,10 @@ class vector
     template <class InputIterator>
     void assign(InputIterator first, InputIterator last)
     {
-      assign__disp(first, last, is_integral<InputIterator>::type());
+      assign__disp(
+        first,
+        last,
+        typename iterator_traits<InputIterator>::iterator_category());
     }
 
     void assign(size_type n, const T& u)
@@ -158,14 +426,24 @@ class vector
       clear();
       if ( capacity() < n )
       {
-        allocator_.deallocate(begin_, capacity_);
-        capacity_ = n;
-        begin_= allocator_.allocate(n);
+        alloc_.deallocate(begin_, capacity_);
+        capacity_ = 0;
+        begin_ = end_ = nullptr;
+        size_type new_capacity = detail::vector_allocation_policy(n);
+        allocation_guard g(alloc_, new_capacity);
+        capacity_ = new_capacity;
+        begin_ = end_ = g.get();
+        detail::uninitailized_fill_n_a(begin_, n, u, alloc_);
+        g.commit();
+        end_ += n;
+        return;
       }
-      construct(n, u);
+
+      detail::uninitailized_fill_n_a(begin_, n, u, alloc_);
+      end_ = begin_ + n;
     }
 
-    allocator_type get_allocator() const  { return allocator_; }
+    allocator_type get_allocator() const  { return alloc_; }
 
     ///@}
 
@@ -173,24 +451,37 @@ class vector
 
     template <class InputIterator>
     void assign__disp(InputIterator first, InputIterator last,
-                      const false_type&)
+                      const input_iterator_tag)
     {
       clear();
-      // distance SHOULD be allways positive
-      size_type n = static_cast<size_type>(std::distance(first, last));
-      if ( capacity() < n )
-      {
-        allocator_.deallocate(begin_, capacity_);
-        capacity_ = n;
-        begin_= allocator_.allocate(n);
-      }
-      construct(first, n);
+      for(; first != last; ++first)
+        push_back(*first);
     }
 
-    template <class IntegralType>
-    void assign__disp(IntegralType n, IntegralType x, const true_type&)
+    template <class ForwardIterator, class IteratorCategory>
+    void assign__disp(ForwardIterator first, ForwardIterator last,
+                      const IteratorCategory)
     {
-      assign(static_cast<size_type>(n), static_cast<value_type>(x));
+      clear();
+      // distance SHOULD always be positive
+      size_type n = static_cast<size_type>(distance(first, last));
+      if ( capacity() < n )
+      {
+        alloc_.deallocate(begin_, capacity_);
+        capacity_ = 0;
+        begin_ = end_ = nullptr;
+        size_type new_capacity = detail::vector_allocation_policy(n);
+        allocation_guard g(alloc_, new_capacity);
+        capacity_ = new_capacity;
+        begin_ = end_ = g.get();
+        detail::uninitailized_copy_a(first, last, begin_, alloc_);
+        g.commit();
+        end_ += n;
+        return;
+      }
+
+      detail::uninitailized_copy_a(first, last, begin_, alloc_);
+      end_ = begin_ + n;
     }
 
   public:
@@ -217,7 +508,7 @@ class vector
     ///\name  capacity [23.2.6.2]
 
     size_type size()      const { return static_cast<size_type>(end_- begin_); }
-    size_type max_size()  const { return allocator_.max_size(); }
+    size_type max_size()  const { return alloc_.max_size(); }
     size_type capacity()  const { return capacity_; }
     bool      empty()     const { return begin_ == end_; }
 
@@ -271,7 +562,7 @@ class vector
       {
         old_mem = begin_;
         capacity_ = n + capacity_factor();
-        const iterator new_mem = allocator_.allocate(capacity_);
+        const iterator new_mem = alloc_.allocate(capacity_);
         new_end = new_mem + difference_type(new_end - old_mem);
         //new_end += difference_type(new_mem - old_mem);        // dangerous alignment
         iterator dest = begin_ = new_mem;
@@ -284,14 +575,14 @@ class vector
       iterator r_dest = end_ = new_end;
       while ( tail_size-- )
         move(--r_dest, --r_src);
-      if ( old_mem ) allocator_.deallocate(old_mem, old_capacity);
+      if ( old_mem ) alloc_.deallocate(old_mem, old_capacity);
       return r_dest;
     }
 
     iterator insert__impl(iterator position, size_type n, const T& x)
     {
       iterator r_dest = insert__blank_space(position, n);
-      while ( n-- ) allocator_.construct(--r_dest, x);
+      while ( n-- ) alloc_.construct(--r_dest, x);
       return r_dest;
     }
 
@@ -307,7 +598,7 @@ class vector
                          InputIterator last, const random_access_iterator_tag &)
     {
       position = insert__blank_space(position, static_cast<size_type>(last - first));
-      while ( first != last ) allocator_.construct(--position, *--last);
+      while ( first != last ) alloc_.construct(--position, *--last);
     }
 
     template <class InputIterator>
@@ -334,10 +625,10 @@ class vector
     void push_back(const T& x)
     {
       if ( size() == capacity() ) realloc(capacity_factor());
-      allocator_.construct(end_++, x);
+      alloc_.construct(end_++, x);
     }
 
-    void pop_back() __ntl_nothrow { allocator_.destroy(--end_); }
+    void pop_back() __ntl_nothrow { alloc_.destroy(--end_); }
 
     iterator insert(iterator position, const T& x)
     {
@@ -359,7 +650,7 @@ class vector
     iterator erase(iterator position) __ntl_nothrow
     {
       // return erase(position, position + 1);
-      allocator_.destroy(position);
+      alloc_.destroy(position);
       --end_;
       iterator i = position;
       do move(i, i + 1); while ( ++i != end_ );
@@ -369,7 +660,7 @@ class vector
     __forceinline
     iterator erase(iterator first, iterator last) __ntl_nothrow
     {
-      for ( iterator i = last; i != first;  ) allocator_.destroy(--i);
+      for ( iterator i = last; i != first;  ) alloc_.destroy(--i);
       iterator const tail = first;
       for ( ; last != end_; ++first, ++last ) move(first, last);
       end_ = first;
@@ -388,7 +679,7 @@ class vector
     {
       difference_type n = end_ - begin_;
       end_ = begin_;
-      while ( n ) allocator_.destroy(begin_ + --n);
+      while ( n ) alloc_.destroy(begin_ + --n);
     }
 
     ///@}
@@ -414,15 +705,15 @@ class vector
     }
 
     vector(pointer first, size_type n, bool, const Allocator& a = Allocator())
-    : allocator_(a), begin_(first), end_(last), capacity(last - first) {}
+    : alloc_(a), begin_(first), end_(last), capacity(last - first) {}
   //end extension
   #endif
 
+    mutable allocator_type  alloc_;
+
+    size_type       capacity_;
     pointer         begin_;
     pointer         end_;
-    size_type       capacity_;
-
-    mutable allocator_type  allocator_;
 
     // "stdexcept.hxx" includes this header
     // hack: MSVC doesn't look inside function body
@@ -433,21 +724,21 @@ class vector
 
     void move(const iterator  to, const iterator from) const
     {
-      allocator_.construct(to, *from);
-      allocator_.destroy(from);
+      alloc_.construct(to, *from);
+      alloc_.destroy(from);
     }
 
 
     void realloc(size_type n) __ntl_throws(bad_alloc)
     {
-      const iterator new_mem = allocator_.allocate(n);
+      const iterator new_mem = alloc_.allocate(n);
       const size_type old_capacity = capacity_;
       capacity_ = n;
       iterator dest = new_mem;
       // this is safe for begin_ == 0 && end_ == 0, but keep vector() coherent
       for ( iterator src = begin_; src != end_; ++src, ++dest )
         move(dest, src);
-      if ( begin_ ) allocator_.deallocate(begin_, old_capacity);
+      if ( begin_ ) alloc_.deallocate(begin_, old_capacity);
       begin_ = new_mem;
       end_ = dest;
     }
