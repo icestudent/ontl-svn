e6781376dbdef1dc3905bbd0b266752e0f056a13
freak.fsfoundry.org <freak.fsfoundry.org@2fbf614b-5b33-0410-b9e2-9946931d7d41>
2008-08-26 15:37:10 +0000

    Compile-time generates the best version of detail::ispo2() and selects best version of detail::nlpo2(). Both used by vector_allocation_policy::aggressive().

--- ntl/stlx/vector.hxx
+++ ntl/stlx/vector.hxx
@@ -55,15 +55,49 @@ namespace detail
 // Is Power Of 2, optionally used in
 // vector_allocation_policy().
 // Ref: http://www.aggregate.org/MAGIC/#Is%20Power%20of%202
-inline bool ispo2(uint64_t n) __ntl_nothrow
+template <class UnsignedIntegerType>
+inline bool ispo2(UnsignedIntegerType n) __ntl_nothrow
 {
+  STATIC_ASSERT(is_integral<UnsignedIntegerType>::value && is_unsigned<UnsignedIntegerType>::value);
   return ((n&(n-1)) == 0) ? true : false;
 }
 
 // Next Largest Power of 2, optionally used in
 // vector_allocation_policy().
 // Ref: http://www.aggregate.org/MAGIC/#Next%20Largest%20Power%20of%202
-inline uint64_t nlpo2(uint64_t n) __ntl_nothrow
+template <class UnsignedIntegerType>
+typename enable_if<sizeof(UnsignedIntegerType) == 1, UnsignedIntegerType>::type
+  nlpo2_impl(UnsignedIntegerType n) __ntl_nothrow
+{
+  n |= (n >> 1);
+  n |= (n >> 2);
+  n |= (n >> 4);
+  return(++n);
+}
+template <class UnsignedIntegerType>
+typename enable_if<sizeof(UnsignedIntegerType) == 2, UnsignedIntegerType>::type
+  nlpo2_impl(UnsignedIntegerType n) __ntl_nothrow
+{
+  n |= (n >> 1);
+  n |= (n >> 2);
+  n |= (n >> 4);
+  n |= (n >> 8);
+  return(++n);
+}
+template <class UnsignedIntegerType>
+typename enable_if<sizeof(UnsignedIntegerType) == 4, UnsignedIntegerType>::type
+  nlpo2_impl(UnsignedIntegerType n) __ntl_nothrow
+{
+  n |= (n >> 1);
+  n |= (n >> 2);
+  n |= (n >> 4);
+  n |= (n >> 8);
+  n |= (n >> 16);
+  return(++n);
+}
+template <class UnsignedIntegerType>
+typename enable_if<sizeof(UnsignedIntegerType) == 8, UnsignedIntegerType>::type
+  nlpo2_impl(UnsignedIntegerType n) __ntl_nothrow
 {
   n |= (n >> 1);
   n |= (n >> 2);
@@ -72,6 +106,13 @@ inline uint64_t nlpo2(uint64_t n) __ntl_nothrow
   n |= (n >> 16);
   n |= (n >> 32);
   return(++n);
+}
+
+template <class UnsignedIntegerType>
+inline UnsignedIntegerType nlpo2(UnsignedIntegerType n) __ntl_nothrow
+{
+  STATIC_ASSERT(is_integral<UnsignedIntegerType>::value && is_unsigned<UnsignedIntegerType>::value);
+  return nlpo2_impl(n);
 }
 
 template <class NotUsed>
@@ -104,7 +145,7 @@ inline vector_allocation_policy::policy_t
 // static
 inline size_t vector_allocation_policy::aggressive(size_t min)
 {
-  if(min <= 4)
+  if(min <= sizeof(void*))
     return min;
   return detail::ispo2(min) ? min : size_t(detail::nlpo2(min));
 }
