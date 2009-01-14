7a47fcd701e341b6831ee3e133d355cc66b9d4a0
icestudent <icestudent@2fbf614b-5b33-0410-b9e2-9946931d7d41>
2009-01-14 23:03:56 +0000

    gcc updated to the 4.3 again

--- config/stlxdef.hxx
+++ config/stlxdef.hxx
@@ -34,8 +34,9 @@
 
 #elif defined(__GNUC__)
 // GCC
-  #if !(__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 2)
-  # error upgrade GCC at least to the GCC 4.2
+  #if !(__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
+  // compilers prior to 4.3 have no the type traits support
+  # error upgrade GCC at least to the GCC 4.3
   #endif
 
   #include "gcc.hxx"
