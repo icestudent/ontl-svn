ebc678095aaf2207e76d891bc49963f1e0f35283
icestudent <icestudent@2fbf614b-5b33-0410-b9e2-9946931d7d41>
2009-01-14 11:44:44 +0000

    f

---
+++ spp/args.hxx
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2008 fr3@K <freak@fsfoundry.org>
+ *
+ * When this software is distributed and/or used with Open NTL, the authors
+ * grant users of this software the choice to either accept the succeeding
+ * license or the license Open NTL is licensed under.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef NTL_SPP_ARGS_HXX
+#define NTL_SPP_ARGS_HXX
+
+#include "control.hxx"
+#include "loop.hxx"
+#include "math.hxx"
+
+// !fr3@K!
+// === implementation detail ===
+#define NTL_SPP_DETAIL_ARG_FIRST(n, prefix) \
+  prefix##n
+#define NTL_SPP_DETAIL_ARG_OTHER(n, prefix) \
+  NTL_SPP_CAT(NTL_SPP_COMMA prefix, n)
+
+#define NTL_SPP_DETAIL_TARG_FIRST(n, p) \
+  T##n p##n
+#define NTL_SPP_DETAIL_TARG_OTHER(n, p) \
+  NTL_SPP_CAT(NTL_SPP_COMMA T, n) p##n
+
+#define NTL_SPP_DETAIL_AARG_FIRST(n, p) \
+  A##n p##n
+#define NTL_SPP_DETAIL_AARG_OTHER(n, p) \
+  NTL_SPP_CAT(NTL_SPP_COMMA A, n) p##n
+
+// !fr3@K!
+// === public interface ===
+
+// NTL_SPP_ARGS
+//    Output comma-separated symbols, as in argument-list format.
+//
+//    NTL_SPP_ARGS outputs symbols prefixed with `prefix` and postfixed
+//    with a index value, which starts at `init` and incremented by 1,
+//    `count` times.
+//
+// Example:
+//    // example.cpp
+//
+//    NTL_SPP_ARGS(0, 3, foo)
+//    NTL_SPP_ARGS(3, 2, bar)
+//    NTL_SPP_ARGS(5, 1, faux)
+//
+//    #if(0)
+//
+//    // GCC: g++ -E example.cpp
+//    // MSVC: cl /E example.cpp
+//    // Output:
+//    foo0, foo1, foo2
+//    bar3, bar4
+//    faux5
+//
+//    #endif
+
+#define NTL_SPP_ARGS(init, count, prefix) \
+  NTL_SPP_IF(count) \
+  ( \
+    NTL_SPP_DETAIL_ARG_FIRST(init, prefix) \
+  ) \
+  NTL_SPP_IF_LT(1, count) \
+  ( \
+    NTL_SPP_LOOP(NTL_SPP_INC(init), NTL_SPP_DEC(count), NTL_SPP_DETAIL_ARG_OTHER, prefix) \
+  )
+
+/**
+ *	@brief Produces comma-separated function parameters of type A
+ *  Example:
+ *  \code
+    NTL_SPP_AARGS(1,2,a)
+    'A1 a1, A2 p2'
+    \endcode
+ **/
+#define NTL_SPP_AARGS(init, count, ArgName) \
+  NTL_SPP_IF(count) \
+  ( \
+  NTL_SPP_DETAIL_AARG_FIRST(init, ArgName) \
+  ) \
+  NTL_SPP_IF_LT(1, count) \
+  ( \
+  NTL_SPP_LOOP(NTL_SPP_INC(init), NTL_SPP_DEC(count), NTL_SPP_DETAIL_AARG_OTHER, ArgName) \
+  )
+
+#endif // !NTL_SPP_ARGS_HXX
---
+++ spp/loop.hxx
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2008 fr3@K <freak@fsfoundry.org>
+ *
+ * When this software is distributed and/or used with Open NTL, the authors
+ * grant users of this software the choice to either accept the succeeding
+ * license or the license Open NTL is licensed under.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef NTL_SPP_LOOP_HXX
+#define NTL_SPP_LOOP_HXX
+
+#include "utility.hxx"
+#include "math.hxx"
+
+// !fr3@K!
+// === implementation detail ===
+#define NTL_SPP_DETAIL_LOOP_0(init, macro, aux)
+
+#define NTL_SPP_DETAIL_LOOP_1(init, macro, aux) \
+  macro(init, aux)
+
+#define NTL_SPP_DETAIL_LOOP_2(init, macro, aux) \
+  NTL_SPP_DETAIL_LOOP_1(init, macro, aux) \
+  macro(NTL_SPP_ADD(init, 1), aux)
+
+#define NTL_SPP_DETAIL_LOOP_3(init, macro, aux) \
+  NTL_SPP_DETAIL_LOOP_2(init, macro, aux) \
+  macro(NTL_SPP_ADD(init, 2), aux)
+
+#define NTL_SPP_DETAIL_LOOP_4(init, macro, aux) \
+  NTL_SPP_DETAIL_LOOP_3(init, macro, aux) \
+  macro(NTL_SPP_ADD(init, 3), aux)
+
+#define NTL_SPP_DETAIL_LOOP_5(init, macro, aux) \
+  NTL_SPP_DETAIL_LOOP_4(init, macro, aux) \
+  macro(NTL_SPP_ADD(init, 4), aux)
+
+
+
+// !fr3@K!
+// === public interface ===
+
+// NTL_SPP_LOOP
+//    Iterative invocations of user specified macro function.
+//
+//    NTL_SPP_LOOP invokes user specified macro function iteratively
+//    `count` times with two arguments. The first argument is an index,
+//    which starts as user specified value - `init`. The index value is
+//    incremented by 1 on every invocation of `macro`. The second
+//    argument used to invoke `macro` is `aux`, which is specified by
+//    user.
+//
+// Parameters:
+//    init:   Initail value of first argument used to invoke `macro`.
+//    count:  Number of invocations of `macro`.
+//    macro:   A SPP macro-function in the form of:
+//              macro(n, aux)
+//    aux:    Auxiliary argument for `macro`.
+//
+// Example:
+//    // example.cpp
+//    #define macro(n, aux) NTL_SPP_CAT(aux, n)
+//
+//    NTL_SPP_LOOP(0, 3, macro, foo)
+//    NTL_SPP_LOOP(3, 2, macro, bar)
+//    NTL_SPP_LOOP(5, 1, macro, faux)
+//
+//    #if(0)
+//
+//    // GCC: g++ -E example.cpp
+//    // MSVC: cl /E example.cpp
+//    // Output:
+//    foo0 foo1 foo2
+//    bar3 bar4
+//    faux5
+//
+//    #endif
+#define NTL_SPP_LOOP(init, count, macro, aux) \
+  NTL_SPP_CAT(NTL_SPP_DETAIL_LOOP_, count)(init, macro, aux)
+
+#endif // !NTL_SPP_LOOP_HXX
