96c28461d05e73496aec16b94a2ceb7bacc3aa21
icestudent <icestudent@2fbf614b-5b33-0410-b9e2-9946931d7d41>
2009-03-09 18:24:27 +0000

    issue #27 fix

--- stlx/std/string.hxx
+++ stlx/std/string.hxx
@@ -412,7 +412,9 @@ public:
     const_reference operator[](size_type pos) const
     {
       static const charT zero_char = charT();
-      return pos < str.size() ? *(begin() + pos) : zero_char;
+      if(pos < str.size())
+        return str[pos];
+      return zero_char;
     }
 
     /// 1 Returns: If pos < size(), returns *(begin() + pos).
